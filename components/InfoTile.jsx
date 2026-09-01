fc2dd3be4ea04b896cad4ea84e7193d100012ed8
Fauxdeedle <fauxdeedle@gmail.com>
2026-09-01 15:57:12 -0700

    fixed highlight-card to fit to hight

--- components/InfoTile.jsx
+++ components/InfoTile.jsx
@@ -4,7 +4,7 @@ export default function InfoTile({ label, value, className = "", ...rest }) {
   return (
     <div className={`${styles.tile} ${className}`} {...rest}>
       <div className={styles.label}>{label}</div>
-      <div className={styles.value}>{value}</div>
+      <h4 className={styles.value}>{value}</h4>
     </div>
   );
 }
