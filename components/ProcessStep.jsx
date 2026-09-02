6d9b3837b26854c342d6cfaf8a3bf710c803b3b4
Fauxdeedle <fauxdeedle@gmail.com>
2026-09-02 16:27:00 -0700

    updated AYZ.AI project and made it show up on the homepage

--- components/ProcessStep.jsx
+++ components/ProcessStep.jsx
@@ -3,14 +3,14 @@ import styles from "./ProcessStep.module.css";
 export default function ProcessStep({ title, description, image, className = "", ...rest }) {
   return (
     <div className={`${styles.step} ${className}`} {...rest}>
-      <div className={styles.text}>
-        <span className={styles.title}>{title}</span>
-        <span className={styles.description}>{description}</span>
-      </div>
       {image && (
         // eslint-disable-next-line @next/next/no-img-element
         <img src={image} alt={title} className={styles.image} />
       )}
+      <div className={styles.text}>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.description}>{description}</span>
+      </div>
     </div>
   );
 }
